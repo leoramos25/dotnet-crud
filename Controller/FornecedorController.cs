13e15c5f587eeb783cc4a3072f02bbca201daa77
Leonardo Ramos de Sousa <leoramosdsousa@gmail.com>
2023-10-02 23:06:46 -0300

    finalizando crud

--- Controller/FornecedorController.cs
+++ Controller/FornecedorController.cs
@@ -30,4 +30,61 @@ public class FornecedorController : ControllerBase
 
         return fornecedor;
     }
+
+    [HttpPost]
+    public async Task<ActionResult<Fornecedor>> CriarFornecedor(Fornecedor fornecedor)
+    {
+        _context.Fornecedores.Add(fornecedor);
+        await _context.SaveChangesAsync();
+
+        return CreatedAtAction("BuscarFornecedorPorId", new { id = fornecedor.Id }, fornecedor);
+    }
+
+    [HttpPut]
+    public async Task<ActionResult<Fornecedor>> AtualizarForncedor(Guid id, Fornecedor fornecedor)
+    {
+        if (id != fornecedor.Id)
+        {
+            return BadRequest();
+        }
+
+        _context.Entry(fornecedor).State = EntityState.Modified;
+
+        try
+        {
+            await _context.SaveChangesAsync();
+        }
+        catch (DbUpdateConcurrencyException)
+        {
+            if (!VerificarSeFornecedorExiste(id))
+            {
+                return NotFound();
+            }
+            else
+            {
+                throw;
+            }
+        }
+
+        return NoContent();
+    }
+
+    public async Task<ActionResult<Fornecedor>> DeletarFornecedor(Guid id)
+    {
+        var fornecedor = await _context.Fornecedores.FindAsync(id);
+        if (fornecedor == null)
+        {
+            return NotFound();
+        }
+
+        _context.Fornecedores.Remove(fornecedor);
+        await _context.SaveChangesAsync();
+
+        return fornecedor;
+    }
+
+    public bool VerificarSeFornecedorExiste(Guid id)
+    {
+        return _context.Fornecedores.Any(fornecedor => fornecedor.Id == id);
+    }
 }
